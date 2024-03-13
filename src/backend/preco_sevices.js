40267b897798e7fd9a82acb4d13f976b3f442e02
anderson souza <andersonfsouza12@gmail.com>
2024-03-13 10:52:10 -0300

    organization of files and new files

--- src/backend/preco_sevices.js
+++ src/backend/preco_sevices.js
@@ -4,6 +4,6 @@ const agendar = document.querySelectorAll('.agendar');
 
 agendar.forEach(function(agenda){
     agenda.addEventListener('click', function(){
-        window.location.href = 'agenda.html';
+        window.location.href = '../agenda/agenda.html';
     });
 });
